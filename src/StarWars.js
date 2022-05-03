28263f7150ffd09e6428bedf4d21ac5882d06538
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-05-02 21:16:00 -0500

    retrieved names adn displayed them on screen

--- src/StarWars.js
+++ src/StarWars.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
 const StarWars = () => {
   const [charLookup, setCharLookup] = useState(1);
+  const [searchResult, setSearchResult] = useState(null)
   const validateInput = (input) => {
-    const numInput = parseInt(input)
-
-     return ((1 <= numInput) && (numInput <= 83) && (numInput !== 17) && (numInput !== 0));
-   
+    const numInput = parseInt(input);
+    return 1 <= numInput && numInput <= 83 && numInput !== 17 && numInput !== 0;
   };
+  const fetchData = async (input) => {
+    if (validateInput(input)){
+        const response = await fetch(`https://swapi.dev/api/people/${input}`)
+        const data = await response.json()
+        return data
+    }
+    alert("invalid input")
+  }
   return (
     <div>
-      <form>
+      <form className="inputForm">
         <label>
+            <span>Enter a Number</span>
           <input
             type={"number"}
             onChange={(e) => setCharLookup(e.target.value)}
@@ -22,12 +30,16 @@ const StarWars = () => {
         <button
           onClick={(e) => {
             e.preventDefault();
-            console.log(validateInput(charLookup));
+            fetchData(charLookup)
+            .then((data) => {
+                setSearchResult(data)
+            });
           }}
         >
           Submit
         </button>
       </form>
+      {searchResult && <p>{searchResult.name}</p>}
     </div>
   );
 };
